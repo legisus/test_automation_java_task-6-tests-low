5f763b3565915018bb553d367d3ec3fdd4f88652
Mykola <legisus@gmail.com>
2021-07-28 08:57:33 +0200

    1

--- src/main/java/com/epam/test/automation/java/practice6/Employee.java
+++ src/main/java/com/epam/test/automation/java/practice6/Employee.java
@@ -1,9 +1,61 @@
 package com.epam.test.automation.java.practice6;
 
+import java.math.BigDecimal;
+
 /**
  * <summary>
  * Implement code according to description of task.
  * </summary>
  */
 public class Employee {
+    private String lastName;
+    private BigDecimal salary;
+    private BigDecimal bonus;
+
+    public Employee(String lastName, BigDecimal salary) {
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public BigDecimal getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(BigDecimal bonus) {
+        this.bonus = bonus;
+    }
+
+    public BigDecimal toPay(){
+        BigDecimal value = new BigDecimal(0);
+        value.add(salary);
+        value.add(bonus);
+        return value;
+    }
+
+
 }
+
