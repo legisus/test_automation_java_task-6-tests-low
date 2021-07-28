f5789359005bcb2f1cbda69495fbb6c333e48b5b
Mykola <legisus@gmail.com>
2021-07-28 09:41:00 +0200

    2

--- src/main/java/com/epam/test/automation/java/practice6/Manager.java
+++ src/main/java/com/epam/test/automation/java/practice6/Manager.java
@@ -1,9 +1,28 @@
 package com.epam.test.automation.java.practice6;
 
+import java.math.BigDecimal;
+
 /**
  * <summary>
  * Implement code according to description of task.
  * </summary>
  */
-public class Manager {
+public class Manager extends Employee{
+    private int clientAmount;
+
+    public Manager(String lastName, BigDecimal salary, int clientAmount) {
+        super(lastName, salary);
+        this.clientAmount = clientAmount;
+    }
+
+    @Override
+    public void setBonus(BigDecimal bonus) {
+        BigDecimal b1 = new BigDecimal(500);
+        BigDecimal b2 = new BigDecimal(1000);
+        super.setBonus(bonus);
+        if (clientAmount > 100)
+            bonus = bonus.add(b1);
+        if (clientAmount > 150)
+            bonus = bonus.add(b2);
+    }
 }
--- src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
+++ src/main/java/com/epam/test/automation/java/practice6/SalesPerson.java
@@ -1,9 +1,36 @@
 package com.epam.test.automation.java.practice6;
 
+import java.math.BigDecimal;
+
 /**
  * <summary>
  * Implement code according to description of task.
  * </summary>
  */
-public class SalesPerson {
+public class SalesPerson extends Employee{
+    private int percent;
+    public SalesPerson(String lastName, BigDecimal salary, int percent) {
+        super(lastName, salary);
+        this.percent = percent;
+    }
+
+    public int getPercent() {
+        return percent;
+    }
+
+    public void setPercent(int percent) {
+        this.percent = percent;
+    }
+
+    @Override
+    public void setBonus(BigDecimal bonus) {
+        super.setBonus(bonus);
+        BigDecimal b1 = new BigDecimal(2);
+        BigDecimal b2 = new BigDecimal(3);
+        if(percent > 100)
+            bonus = bonus.multiply(b1);
+        if (percent > 200)
+            bonus = bonus.multiply(b2);
+
+    }
 }
